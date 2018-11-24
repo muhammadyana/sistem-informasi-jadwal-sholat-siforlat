182c941a3e6906b814699102390c75c27be23532
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 21:46:53 +0700

    :art: add fonts in assets :zap:

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Siforlat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << "#{Rails.root}/vendor/assets/fonts}"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts}"
 		config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
