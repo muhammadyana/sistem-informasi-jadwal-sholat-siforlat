032eb5b9ea3433d3e9b69a7586b8283d41751a99
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 22:09:38 +0700

    :ambulance: clean code :tada:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,5 +106,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_controller.default_url_options = { host: "https://siforlat.herokuapp.com" }
 end
-Rails.application.routes.default_url_options[:host] = 'https://siforlat.herokuapp.com'
