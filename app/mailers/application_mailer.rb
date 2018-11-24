58bc607d2afc1f2543c92c33a58519bec8c6b3b3
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 18:07:20 +0700

    :fire: update default email sending :zap:

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'SIFORLAT - Sistem Informasi Jadwal Sholat <no-reply@siforlat.com>'
   layout 'mailer'
 end
