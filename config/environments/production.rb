7ca765adfb25c55bbe70fcc46924b0c3e4710b8e
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 17:34:48 +0700

    :ambulance: update default url in production :tada:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -103,3 +103,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.routes.default_url_options[:host] = 'https://siforlat.herokuapp.com'
