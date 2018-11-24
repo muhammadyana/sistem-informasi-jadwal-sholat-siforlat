8a07327dfe8f078c752af6e0e9eea0b0f42da233
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 17:11:43 +0700

    :ambulance: add username permit params :zap:

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :mobile_phone, :address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :mobile_phone, :address])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
