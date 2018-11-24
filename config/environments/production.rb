49e569a7c0f5b0f8dc73f28033e23db1270511d8
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 22:44:47 +0700

    :ambulance: fixed default url :zap:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,5 +107,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_controller.default_url_options = { host: "https://siforlat.herokuapp.com" }
-  config.default_url_options[:host] = 'http://dashboard.medic-trust.com'
+  config.action_mailer.default_url_options = { host: 'https://siforlat.herokuapp.com' }
+  # config.default_url_options[:host] = 'http://dashboard.medic-trust.com'
 end
