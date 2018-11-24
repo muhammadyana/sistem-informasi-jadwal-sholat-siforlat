204038a309bf34040ef2fb4ac9708a14e2fc0c38
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 14:31:55 +0700

    :tractor: AddDeviseTokenAuthFieldsUsers :tada:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_064105) do
+ActiveRecord::Schema.define(version: 2018_11_24_073058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,8 +78,12 @@ ActiveRecord::Schema.define(version: 2018_11_24_064105) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.json "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
