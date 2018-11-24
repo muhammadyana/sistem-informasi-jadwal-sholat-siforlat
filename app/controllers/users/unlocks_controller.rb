ea2482c7f355244074ff530123e23d435d263069
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 18:53:04 +0700

    :art: change layout for devise :lipstick:

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
+  layout "layouts/authentication"
   # GET /resource/unlock/new
   # def new
   #   super
