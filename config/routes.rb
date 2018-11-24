c8ebb1d85a4875eaa59f260d24fb445d464a2247
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 14:30:38 +0700

    :ambulance: fixed route for api

--- config/routes.rb
+++ config/routes.rb
@@ -13,22 +13,23 @@ Rails.application.routes.draw do
   get 'pages/index'
   root 'pages#index'
   
-  # mount_devise_token_auth_for 'User', at: '/api/v1/users', path: 'users_api', controllers: {
-  #   registrations:  'api/v1/registrations', 	as: :new_user_api_registration_path,
-  #   sessions:  			'api/v1/sessions', 				as: :user_api_session_path,
-  #   passwords:  		'api/v1/passwords', 			as: :new_user_api_password_path
-  # }
+		  
 
-  # namespace :api do
-  #   namespace :v1, defaults: { format: :json } do
-  #     devise_scope :user do
-  #       get :status, to: 'api#status'
-  #       resources :users, only: :show
-  #       resource :user, only: :update do
-  #         get :profile
-  #       end
-  #     end
-  #   end
-  # end
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+    	mount_devise_token_auth_for 'User', at: 'users', controllers: {
+		    registrations:  'api/v1/registrations', 	as: :new_user_api_registration_path,
+		    sessions:  			'api/v1/sessions', 				as: :user_api_session_path,
+		    passwords:  		'api/v1/passwords', 			as: :new_user_api_password_path
+		  }
+      devise_scope :user do
+        get :status, to: 'api#status'
+        resources :users, only: :show
+        resource :user, only: :update do
+          get :profile
+        end
+      end
+    end
+  end
   
 end
