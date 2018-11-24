438b53a532f953cb309e485691de074fe0980789
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-24 14:45:06 +0700

    :tractor: add additional methode fo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, :confirmable
+
+
+  include DeviseTokenAuth::Concerns::User
+
+  validates :uid, uniqueness: { scope: :provider }
+
+  before_validation :init_uid
+
+  def full_name
+    return username unless first_name.present?
+    "#{first_name} #{last_name}"
+  end
+
+  def self.from_social_provider(provider, user_params)
+    where(provider: provider, uid: user_params['id']).first_or_create do |user|
+      user.password = Devise.friendly_token[0, 20]
+      user.assign_attributes user_params.except('id')
+    end
+  end
+
+  private
+
+  def uses_email?
+    provider == 'email' || email.present?
+  end
+
+  def init_uid
+    self.uid = email if uid.blank? && provider == 'email'
+  end
+  
 end
