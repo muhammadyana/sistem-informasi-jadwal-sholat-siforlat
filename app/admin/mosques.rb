74e08a72e1a3bccc98caf16dbf65574c2effba1a
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-25 22:50:03 +0700

    :tractor: add permit params for mosque model :tada:

--- app/admin/mosques.rb
+++ app/admin/mosques.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Mosque do
 # See permitted parameters documentation:
+permit_params :name, :address, :latitude, :longitude, :slug, :user_id
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
