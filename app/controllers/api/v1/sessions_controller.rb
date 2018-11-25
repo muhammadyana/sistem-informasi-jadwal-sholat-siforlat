fdb606cc709b5f2269d0e8a398e7fe36dad0e41b
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-25 23:54:16 +0700

    :ambulance: fixed error :bug:

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,7 +14,7 @@ module Api
 
       def render_create_success
         # TODO AMS not support withh devise_token_auth, this is i found from github, and it not usually use by AMS 
-        render json: ActiveModel::UserSerializer.new(@resource).as_json
+        render json: UserSerializer.new(@resource).as_json
         # render json: {
         #   status: "success",
         #   user: resource_data(
