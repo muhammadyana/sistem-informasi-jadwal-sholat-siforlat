aa97f7b6d3e958399a6f9d3686c718fa123c6a1a
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-26 00:07:48 +0700

    :ambulance: fixed error friendly_id for active admin :tada:

--- app/admin/mosques.rb
+++ app/admin/mosques.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register Mosque do
 # See permitted parameters documentation:
-permit_params :name, :address, :latitude, :longitude, :slug, :user_id
+	permit_params :name, :address, :latitude, :longitude, :slug, :user_id
+	before_action :only => [:show] do
+	  @mosque = Mosque.friendly.find(params[:id])
+	end
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
@@ -12,5 +15,6 @@ permit_params :name, :address, :latitude, :longitude, :slug, :user_id
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
 
 end
