d8969eafd2acf66e7c997d6a1b86c4b06fac9ac1
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-27 23:55:55 +0700

    :newspaper: add landing css :tada:

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,16 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
+//= require argon-landing.min
+//= require notie.min
+//= require flipclock
+//= require moment
+//= require PrayTimes
+
+
+
