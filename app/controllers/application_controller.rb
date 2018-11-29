0f6cd963e34c2b3c986d7c94745bcff466727bd0
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-29 14:22:55 +0700

    :tractor: fixed path for admin :tada:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,17 @@ class ApplicationController < ActionController::Base
 		# end
 		
 		def after_sign_in_path_for(resource)
-			logger.info("======== resource user = #{resource.to_json}")
-			logger.info("======== mosque present? = #{resource.mosque.present?}")
-			# If user dont have a mosque? redirect to create new mosque
-			unless resource.mosque.present?
-				new_user_mosque_path(resource)
+			unless resource.class.name.eql?('AdminUser')
+				logger.info("======== resource user = #{resource.to_json}")
+				logger.info("======== mosque present? = #{resource.mosque.present?}")
+				# If user dont have a mosque? redirect to create new mosque
+				unless resource.mosque.present?
+					new_user_mosque_path(resource)
+				else
+					stored_location_for(resource) || dashboard_path
+				end
 			else
-				stored_location_for(resource) || dashboard_path
+				admin_root_path
 			end
-      
     end
 end
