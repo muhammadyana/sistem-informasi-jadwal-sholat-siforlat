adf472396928b6b4b59ca8d6ebc801ca2bada90b
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-30 22:45:52 +0700

    :hammer: add = gravatar_for helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def gravatar_for(user, class_names=nil, options = { size: 500 })
+  	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  	size = options[:size]
+  	gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  	image_tag(gravatar_url, alt: user.full_name, class: class_names)
+	end
 end
