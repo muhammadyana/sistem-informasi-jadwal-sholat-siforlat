858e92e12f4fb8509c999d445ad1a3021b89451d
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-03 17:32:06 +0700

    :art: change mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => 'gmail.com',
+   :domain               => 'siforlat.com',
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
