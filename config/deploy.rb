8288258b2538e072e40b990a16aaf2964874c35c
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-03 13:17:03 +0700

    :tractor: add deploy configuration :tada:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,143 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 		"siforlat"
+set :repo_url, 				"git@github.com:muhammadyana/sistem-informasi-jadwal-sholat-siforlat.git"
+
+set :user,            'rails'
+
+set :puma_env, 'production'
+set :puma_threads, [1, 16]
+set :puma_workers, 1
+
+set :rvm_ruby_version, '2.4.1'
+
+set :ssh_options, 				{ forward_agent: true, auth_methods: %w(publickey) }
+set :stage,           		:production
+set :deploy_via,      		:remote_cache
+set :deploy_to,      		 "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       		"unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      		"#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        		"#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, 		"#{release_path}/log/puma.error.log"
+set :puma_error_log,  		"#{release_path}/log/puma.access.log"
+set :ssh_options,     		{ forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, 		true
+set :puma_worker_timeout, nil
+
+set :linked_files, %w{.env}
+set :linked_dirs, %w{log tmp/cache tmp/sockets tmp/pids public/assets public/uploads }
+
+
+
+# whenever cron setting
+# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+
+SSHKit.config.command_map[:rake] = 'bundle exec rake'
+SSHKit.config.command_map[:runner] = 'bundle exec rails runner'
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  # desc "Load database with seed data"
+  # task :seed do
+  #   run "cd #{deploy_to}/current; /usr/bin/env bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  # end
+
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     invoke 'delayed_job:restart'
+  #     # Here we can do anything such as:
+  #     # within release_path do
+  #     #   execute :rake, 'cache:clear'
+  #     # end
+  #   end
+  # end
+
+  # after 'deploy:publishing', 'deploy:restart'
+  # namespace :deploy do
+  #   task :restart do
+  #     invoke 'delayed_job:restart'
+  #   end
+  # end
+
+  after :publishing, :restart
+# set :puma_threads,    [4, 16]
+# set :puma_workers,    0
+
+# # Don't change these unless you know what you're doing
+# set :pty,             true
+# set :use_sudo,        false
+# set :stage,           :production
+# set :deploy_via,      :remote_cache
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+# ## Linked Files & Directories (Default None):
+# set :linked_files, %w{.env}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# namespace :puma do
+#   desc 'Create Directories for Puma Pids and Socket'
+#   task :make_dirs do
+#     on roles(:app) do
+#       execute "mkdir #{shared_path}/tmp/sockets -p"
+#       execute "mkdir #{shared_path}/tmp/pids -p"
+#     end
+#   end
+
+#   before :start, :make_dirs
+# end
+
+# namespace :deploy do
+#   desc "Make sure local git is in sync with remote."
+#   task :check_revision do
+#     on roles(:app) do
+#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
+#         puts "WARNING: HEAD is not the same as origin/master"
+#         puts "Run `git push` to sync changes."
+#         exit
+#       end
+#     end
+#   end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       invoke 'puma:restart'
+#     end
+#   end
+
+#   before :starting,     :check_revision
+#   after  :finishing,    :compile_assets
+#   after  :finishing,    :cleanup
+#   after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
