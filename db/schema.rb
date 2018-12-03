75b2be7abdafc3e34ec705f4a43eec420b4e27d8
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-03 18:44:50 +0700

    :tratctor: AddSlugToUsers slug:uniq :hammer:'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_153445) do
+ActiveRecord::Schema.define(version: 2018_12_03_114357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,8 +107,10 @@ ActiveRecord::Schema.define(version: 2018_11_25_153445) do
     t.string "uid", default: "", null: false
     t.json "tokens"
     t.string "username"
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
