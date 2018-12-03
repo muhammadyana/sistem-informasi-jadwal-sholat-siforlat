99b6f6f9526c8ba01bb65af4c376a622ca3ab89c
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-03 23:24:58 +0700

    redirect_to new user mosque when mosque is blank

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,14 @@
 class DashboardController < ApplicationController
 	before_action :authenticate_user!
+	before_action :check_user_mosque
+
+
   def index
   end
+
+  private
+  def check_user_mosque
+  	redirect_to new_user_mosque_path(current_user) unless current_user.mosque.present?
+  end
+
 end
