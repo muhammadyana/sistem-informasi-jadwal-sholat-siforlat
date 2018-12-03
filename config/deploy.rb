8c7c073fa454db30b428efca167b01f5010d9bf5
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-03 13:20:20 +0700

    :tractor: add deploy configuration :tada:

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,8 +24,9 @@ set :puma_error_log,  		"#{release_path}/log/puma.access.log"
 set :ssh_options,     		{ forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, 		true
 set :puma_worker_timeout, nil
+set :keep_releases, 5
 
-set :linked_files, %w{.env}
+set :linked_files, %w{.env config/database.yml}
 set :linked_dirs, %w{log tmp/cache tmp/sockets tmp/pids public/assets public/uploads }
 
 
@@ -67,52 +68,6 @@ namespace :deploy do
   # end
 
   after :publishing, :restart
-# set :puma_threads,    [4, 16]
-# set :puma_workers,    0
-
-# # Don't change these unless you know what you're doing
-# set :pty,             true
-# set :use_sudo,        false
-# set :stage,           :production
-# set :deploy_via,      :remote_cache
-# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-# set :puma_access_log, "#{release_path}/log/puma.error.log"
-# set :puma_error_log,  "#{release_path}/log/puma.access.log"
-# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-# set :puma_preload_app, true
-# set :puma_worker_timeout, nil
-# set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-# ## Linked Files & Directories (Default None):
-# set :linked_files, %w{.env}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# namespace :puma do
-#   desc 'Create Directories for Puma Pids and Socket'
-#   task :make_dirs do
-#     on roles(:app) do
-#       execute "mkdir #{shared_path}/tmp/sockets -p"
-#       execute "mkdir #{shared_path}/tmp/pids -p"
-#     end
-#   end
-
-#   before :start, :make_dirs
-# end
-
-# namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
-#     on roles(:app) do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#         puts "WARNING: HEAD is not the same as origin/master"
-#         puts "Run `git push` to sync changes."
-#         exit
-#       end
-#     end
-#   end
 
   desc 'Initial Deploy'
   task :initial do
