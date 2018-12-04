c65ae9ab1cbc8d22db31cee43412e2d93ecc9a12
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-04 22:43:02 +0700

    add_avatar_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_03_135519) do
+ActiveRecord::Schema.define(version: 2018_12_04_154224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_135519) do
     t.json "tokens"
     t.string "username"
     t.string "slug"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true
