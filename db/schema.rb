77f69d126db9c140836fe77a3661b0ad00272222
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-12-04 22:43:55 +0700

    add_avatar_to_mosques

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_154224) do
+ActiveRecord::Schema.define(version: 2018_12_04_154322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_154224) do
     t.string "zipcode"
     t.string "country"
     t.integer "iqomah"
+    t.string "avatar"
     t.index ["slug"], name: "index_mosques_on_slug", unique: true
     t.index ["user_id"], name: "index_mosques_on_user_id"
   end
