cf539d618e5f735d4992321b424eb0cafcc6f881
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-04-27 14:02:09 +0700

    :ambulance: fixed some bug :bug:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, 		"siforlat"
-set :repo_url, 				"git@github.com:muhammadyana/sistem-informasi-jadwal-sholat-siforlat.git"
+set :application,     "siforlat"
+set :repo_url,        "git@github.com:muhammadyana/sistem-informasi-jadwal-sholat-siforlat.git"
 
 set :user,            "rails"
 
@@ -12,16 +12,16 @@ set :puma_workers, 1
 
 set :rvm_ruby_version, '2.4.1'
 
-set :stage,           		:production
-set :deploy_via,      		:remote_cache
-set :deploy_to,      		 "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       		"unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      		"#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        		"#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, 		"#{release_path}/log/puma.error.log"
-set :puma_error_log,  		"#{release_path}/log/puma.access.log"
-set :ssh_options,     		{ forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, 		true
+set :stage,               :production
+set :deploy_via,          :remote_cache
+set :deploy_to,          "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,           "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,          "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,            "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log,     "#{release_path}/log/puma.error.log"
+set :puma_error_log,      "#{release_path}/log/puma.access.log"
+set :ssh_options,         { forward_agent: true, auth_methods: %w(publickey) }
+set :puma_preload_app,    true
 set :puma_worker_timeout, nil
 set :keep_releases, 5
 
