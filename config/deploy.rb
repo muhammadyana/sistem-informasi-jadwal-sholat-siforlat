5cdeff10b6645e53e9966f8bd09cbe2746ec1c02
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2019-05-24 13:36:11 +0700

    :bug: fixed bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application,     "siforlat"
-set :repo_url,        "git@github.com:muhammadyana/sistem-informasi-jadwal-sholat-siforlat.git"
-
-set :user,            "rails"
-
-set :puma_env, 'production'
-set :puma_threads, [1, 16]
-set :puma_workers, 1
-
-set :rvm_ruby_version, '2.4.1'
-
-set :stage,               :production
-set :deploy_via,          :remote_cache
-set :deploy_to,          "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,           "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,          "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,            "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log,     "#{release_path}/log/puma.error.log"
-set :puma_error_log,      "#{release_path}/log/puma.access.log"
-set :ssh_options,         { forward_agent: true, auth_methods: %w(publickey) }
-set :puma_preload_app,    true
-set :puma_worker_timeout, nil
-set :keep_releases, 5
-
-set :linked_files, %w{.env config/master.key}
-set :linked_dirs, %w{log tmp/cache tmp/sockets tmp/pids public/assets public/uploads }
-
-
-
-# whenever cron setting
-# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
-SSHKit.config.command_map[:rake] = 'bundle exec rake'
-SSHKit.config.command_map[:runner] = 'bundle exec rails runner'
+set :application,       "siforlat"
+set :repo_url,          "git@github.com:muhammadyana/sistem-informasi-jadwal-sholat-siforlat.git"
+set :rvm_ruby_version,  '2.4.1'
+set :puma_threads,      [1, 16]
+set :puma_workers,      1
+set :linked_files,      %w{.env config/master.key config/database.yml}
+set :linked_dirs,       %w{log tmp/cache tmp/sockets tmp/export tmp/pids public/assets public/uploads}
+set :delayed_job_workers, 1
 
 namespace :deploy do
   desc 'Restart application'
@@ -44,30 +18,8 @@ namespace :deploy do
     end
   end
 
-  # desc "Load database with seed data"
-  # task :seed do
-  #   run "cd #{deploy_to}/current; /usr/bin/env bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-  # end
-
-  # after :restart, :clear_cache do
-  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #     invoke 'delayed_job:restart'
-  #     # Here we can do anything such as:
-  #     # within release_path do
-  #     #   execute :rake, 'cache:clear'
-  #     # end
-  #   end
-  # end
-
-  # after 'deploy:publishing', 'deploy:restart'
-  # namespace :deploy do
-  #   task :restart do
-  #     invoke 'delayed_job:restart'
-  #   end
-  # end
-
   after :publishing, :restart
-
+  
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
@@ -75,19 +27,12 @@ namespace :deploy do
       invoke 'deploy'
     end
   end
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       invoke 'puma:restart'
-#     end
-#   end
-
-#   before :starting,     :check_revision
-#   after  :finishing,    :compile_assets
-#   after  :finishing,    :cleanup
-#   after  :finishing,    :restart
 end
+
+# whenever cron setting
+# set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+SSHKit.config.command_map[:rake] = 'bundle exec rake'
+SSHKit.config.command_map[:runner] = 'bundle exec rails runner'
 
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
